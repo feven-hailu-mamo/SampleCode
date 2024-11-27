747ecbdcac9f7f83193334ab4429025fdfe21523
feven-hailu-mamo <56573118+feven-hailu-mamo@users.noreply.github.com>
2024-11-27 11:12:55 -0800

    Update VirtualColumns.sql

--- VirtualColumns.sql
+++ VirtualColumns.sql
@@ -26,27 +26,22 @@ ADD (
 ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/(3[0-2]|[12]?[0-9])$
 
 
---H2 computed columns
+-- Adding the network_int computed column
 ALTER TABLE Network
-ADD COLUMN network_int AS (
-    CAST(
-        (CAST(SUBSTRING_INDEX(cidr, '.', 1) AS INT) * 256 * 256 * 256) +
-        (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 2), '.', -1) AS INT) * 256 * 256) +
-        (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 3), '.', -1) AS INT) * 256) +
-        CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 4), '/', 1) AS INT)
-        AS INT
-    )
-);
+ADD COLUMN network_int INT GENERATED ALWAYS AS (
+    (CAST(SUBSTRING_INDEX(cidr, '.', 1) AS INT) * 256 * 256 * 256) +
+    (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 2), '.', -1) AS INT) * 256 * 256) +
+    (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 3), '.', -1) AS INT) * 256) +
+    CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 4), '/', 1) AS INT)
+) STORED;
 
+-- Adding the broadcast_int computed column
 ALTER TABLE Network
-ADD COLUMN broadcast_int AS (
-    CAST(
-        (CAST(SUBSTRING_INDEX(cidr, '.', 1) AS INT) * 256 * 256 * 256) +
-        (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 2), '.', -1) AS INT) * 256 * 256) +
-        (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 3), '.', -1) AS INT) * 256) +
-        CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 4), '/', 1) AS INT) + 
-        (POWER(2, (32 - CAST(SUBSTRING_INDEX(cidr, '/', -1) AS INT))) - 1)
-        AS INT
-    )
-);
+ADD COLUMN broadcast_int INT GENERATED ALWAYS AS (
+    (CAST(SUBSTRING_INDEX(cidr, '.', 1) AS INT) * 256 * 256 * 256) +
+    (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 2), '.', -1) AS INT) * 256 * 256) +
+    (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 3), '.', -1) AS INT) * 256) +
+    CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(cidr, '.', 4), '/', 1) AS INT) + 
+    (POWER(2, (32 - CAST(SUBSTRING_INDEX(cidr, '/', -1) AS INT))) - 1)
+) STORED;
 
